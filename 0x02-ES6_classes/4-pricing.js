9d7214b34b15a52fdccc06294dbee3134d729120
Ato Toffah <atotoffah@gmail.com>
2024-03-07 17:26:58 +0000

    Task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw TypeError('Currency must be a Currency Object');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+
+    if (typeof conversionRate !== 'number') {
+      throw TypeError('Conversion Rate must be a number');
+    }
+
+    return (amount * conversionRate);
+  }
+}
