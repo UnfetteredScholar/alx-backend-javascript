70a149844daffb6137361d1f391c1514a831f688
Ato Toffah <atotoffah@gmail.com>
2024-03-12 18:43:10 +0000

    Task 0:fix 2

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,7 +3,7 @@ export default function getStudents() {
 
   result.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
   result.push({ id: 2, firstName: 'James', location: 'Columbia' });
-  result.push({ id: 3, firstName: 'Serena', location: 'San Francisco' });
+  result.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
 
   return result;
 }
