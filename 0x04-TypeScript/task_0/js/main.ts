c082acba317cd68af239b90afef400bf0ef390b4
Ato Toffah <atotoffah@gmail.com>
2024-03-13 22:25:01 +0000

    Task 2

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,33 +1,22 @@
-// Write your code in the main.ts file:
-
-// Write an interface named Student that accepts the following elements: firstName(string), lastName(string), age(number), and location(string)
-// Create two students, and create an array named studentArray containing the two variables
-// Using Vanilla Javascript, render a table and for each elements in the array, append a new row to the table
-// Each row should contain the first name of the student and the location
-// Requirements:
-
-// When running, Webpack should return No type errors found.
-// Every variable should use TypeScript when possible.
-
 export interface Student {
-    firstName: string
-    lastName: string
-    age: number
-    location: string
+  firstName: string
+  lastName: string
+  age: number
+  location: string
 };
 
 const student1: Student = {
-    firstName: "Ato",
-    lastName: "Toffah",
-    age: 22,
-    location: "Ghana"
+  firstName: "Ato",
+  lastName: "Toffah",
+  age: 22,
+  location: "Ghana"
 };
 
 const student2: Student = {
-    firstName: "David",
-    lastName: "Arko",
-    age: 12,
-    location: "South Africa"
+  firstName: "David",
+  lastName: "Arko",
+  age: 12,
+  location: "South Africa"
 };
 
 const studentArray: Array<Student> = [student1, student2];
@@ -67,29 +56,29 @@ const styleSheet = `
 `;
 
 export const displayStudents = (students: Array<Student>): void => {
-    const table = document.createElement('table')
-    const header = document.createElement('tr')
-    const col1 = document.createElement('th')
-    col1.innerText = 'FirstName'
-    const col2 = document.createElement('th')
-    col2.innerText = 'Location'
-    header.insertAdjacentElement('beforeend', col1)
-    header.insertAdjacentElement('beforeend', col2)
-    table.insertAdjacentElement('beforeend', header)
+  const table = document.createElement('table')
+  const header = document.createElement('tr')
+  const col1 = document.createElement('th')
+  col1.innerText = 'FirstName'
+  const col2 = document.createElement('th')
+  col2.innerText = 'Location'
+  header.insertAdjacentElement('beforeend', col1)
+  header.insertAdjacentElement('beforeend', col2)
+  table.insertAdjacentElement('beforeend', header)
 
-    for (const student of students) {
-        const row = document.createElement('tr');
-        const name = document.createElement('td');
-        name.innerText = student.firstName;
-        const location = document.createElement('td');
-        location.innerText = student.location;
-        row.insertAdjacentElement('beforeend', name);
-        row.insertAdjacentElement('beforeend', location);
-        table.insertAdjacentElement('beforeend', row);
+  for (const student of students) {
+    const row = document.createElement('tr');
+    const name = document.createElement('td');
+    name.innerText = student.firstName;
+    const location = document.createElement('td');
+    location.innerText = student.location;
+    row.insertAdjacentElement('beforeend', name);
+    row.insertAdjacentElement('beforeend', location);
+    table.insertAdjacentElement('beforeend', row);
 
-    }
+  }
 
-    document.body.insertAdjacentElement('beforeend', table)
+  document.body.insertAdjacentElement('beforeend', table)
 }
 
 
