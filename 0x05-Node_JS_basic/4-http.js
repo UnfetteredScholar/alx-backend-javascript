f8270ea70878cde8e1c211ce5d67833fb24b8f40
Ato Toffah <atotoffah@gmail.com>
2024-04-30 12:38:10 +0000

    Task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,5 @@
 const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = createServer((req, res) => {
@@ -9,4 +8,4 @@ const app = createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname);
+app.listen(port);
