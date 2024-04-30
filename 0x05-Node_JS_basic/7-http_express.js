519b21168bd450d876d62ce823c9413d80933db9
Ato Toffah <atotoffah@gmail.com>
2024-04-30 14:14:19 +0000

    Task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { argv } = require('process');
+const fs = require('fs');
+
+const DATABASE = argv[2];
+const app = express();
+const port = 1245;
+
+function countStudents(path) {
+  const promise = (res, rej) => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        rej(Error('Cannot load the database'));
+      }
+      let result = '';
+      if (data) {
+        let dbData = data.toString().split('\n');
+        dbData = dbData.slice(1, dbData.length - 1);
+        result = `Number of students: ${dbData.length}`;
+        const obj = {};
+        dbData.forEach((element) => {
+          const student = element.split(',');
+          if (!obj[student[3]]) obj[student[3]] = [];
+          obj[student[3]].push(student[0]);
+        });
+        for (const cls in obj) {
+          if (cls) result += `\nNumber of students in ${cls}: ${obj[cls].length}. List: ${obj[cls].join(', ')}`;
+        }
+      }
+      res(result);
+    });
+  };
+
+  return new Promise(promise);
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const students = await countStudents(DATABASE);
+  res.send(students);
+});
+
+app.listen(port);
+module.exports = app;
