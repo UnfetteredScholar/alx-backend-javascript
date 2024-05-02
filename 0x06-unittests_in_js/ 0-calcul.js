75e8de936ad4ac94e6522e4e827ab9026f5cc2e6
Ato Toffah <atotoffah@gmail.com>
2024-05-02 19:31:43 +0000

    Task 0

---
+++ 0x06-unittests_in_js/ 0-calcul.js
@@ -0,0 +1,7 @@
+function calculateNumber(a, b) {
+  const sum = Math.round(a) + Math.round(b);
+
+  return sum;
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/main.js
@@ -0,0 +1,3 @@
+const calculateNumber = require('./ 0-calcul');
+
+console.log(calculateNumber(3.5, 3.6));
