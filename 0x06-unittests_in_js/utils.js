8a80382df54f20268c939bed1cea791507cf3bcc
Ato Toffah <atotoffah@gmail.com>
2024-05-02 20:13:00 +0000

    Task 3

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,14 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    // SUM, SUBTRACT, or DIVIDE
+    let res = 'Error';
+
+    if (type === 'SUM') res = Math.round(a) + Math.round(b);
+    else if (type === 'SUBTRACT') res = Math.round(a) - Math.round(b);
+    else if (type === 'DIVIDE' && Math.round(b) !== 0) res = Math.round(a) / Math.round(b);
+
+    return res;
+  },
+};
+
+module.exports = Utils;
